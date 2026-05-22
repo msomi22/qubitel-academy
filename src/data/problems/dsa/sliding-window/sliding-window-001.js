962a8f1ba5ad0ad3b7cd46ab01cf6921681ffd46
Peter Mwenda <mwendapeter72@gmail.com>
2026-05-22 12:30:16 +0300

    Migrate sliding-window-001 to approved discovered DSA problem

--- src/data/problems/dsa/sliding-window/sliding-window-001.js
+++ src/data/problems/dsa/sliding-window/sliding-window-001.js
@@ -1,8 +1,16 @@
+import { defineProblem } from '../../../../problems/problemAuthoring.js';
 import topic from '../../../banks/dsa/sliding-window.js';
 
-const problem = {
-  ...topic.questions.find((question) => question.id === 'sliding-window-001'),
-  category: 'dsa'
-};
+const legacyProblem = topic.questions.find((question) => question.id === 'sliding-window-001');
+
+const problem = defineProblem({
+  ...legacyProblem,
+  category: 'dsa',
+  metadata: {
+    ...(legacyProblem.metadata || {}),
+    reviewStatus: 'approved',
+    visibility: ['dev', 'prod']
+  }
+});
 
 export default problem;
