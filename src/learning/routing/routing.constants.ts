921200e22259913ca92bd65e676540178b5408f5
Peter Mwenda <mwendapeter72@gmail.com>
2026-06-08 19:10:48 +0300

    feat: add LearningNode routing constants

---
+++ src/learning/routing/routing.constants.ts
@@ -0,0 +1,7 @@
+export const LEARNING_NODE_ROUTING_DEFAULTS = {
+  basePath: '/',
+  includeRoot: true,
+  segmentAttributeKey: 'routeSegment'
+} as const;
+
+export const LEARNING_NODE_ROUTING_SEPARATOR = '/';
